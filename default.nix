60800bd2b2139460c4cf3372628ef9b5fd1a4a32
takagiy <takagiy.4dev@gmail.com>
2020-10-31 00:56:11 +0900

    initial commit

---
+++ default.nix
@@ -0,0 +1,54 @@
+{ lib, config, pkgs, ...}:
+
+with lib;
+
+let
+  cfg = config.programs.fish;
+
+  pluginDrvs = map (name: {
+    name = baseNameOf name;
+    path = fetchGit {
+      url = "https://github.com/${name}.git";
+    };
+  }) cfg.plugins;
+
+  fishPath =
+    if length cfg.plugins == 0 then null
+    else pkgs.buildEnv {
+      name = "fish-plugins";
+      paths = map (drv: drv.path) pluginDrvs;
+      pathsToLink = [ "/conf.d" "/functions" "/completions" ];
+    };
+in
+
+{
+  options.programs.fish.plugins = mkOption {
+    type = with types; listOf str;
+    default = [];
+    example = literalExample ''
+      [
+        "jethrokuan/fzf"
+        "b4b4r07/enhancd"
+      ]
+    '';
+    description = ''
+      List of fish plugins to be installed.
+    '';
+  };
+
+  config = mkIf cfg.enable {
+    programs.fish.interactiveShellInit = optionalString (fishPath != null) ''
+      set -g nixos_fish_plugins ${fishPath}
+      set -p fish_function_path fish_function_path[1] ''$nixos_fish_plugins/functions
+      set -p fish_complete_path fish_complete_path[1] ''$nixos_fish_plugins/completions
+
+      ${concatMapStringsSep "\n" (drv: ''
+        path=${drv.path} package=${drv.name} builtin source ${drv.path}/init.fish 2> /dev/null
+      '') pluginDrvs}
+
+      for file in ''$nixos_fish_plugins/conf.d/*.fish
+          builtin source ''$file 2> /dev/null
+      end
+    '';
+  };
+}
