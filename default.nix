049abdea8312ff93acda55445340da614cc4c2c8
takagiy <takagiy.4dev@gmail.com>
2020-12-15 19:40:32 +0900

    emit <plugin-name>_install event when build derivation, as fisher does

--- default.nix
+++ default.nix
@@ -39,8 +39,9 @@ let
       name = "fish-plugins";
       paths = map (drv: drv.path) pluginDrvs;
       pathsToLink = [ "/conf.d" "/functions" "/completions" ];
+      buildInputs = [ pkgs.fish ];
       postBuild = ''
-        fish -c '
+        HOME=(mktemp -d) fish -c '
 	  ${initScript "$out"}
 	  ${installScript "$out"}
 	'
@@ -63,22 +64,6 @@ in
     '';
   };
 
-  #config = mkIf cfg.enable {
-  #  programs.fish.interactiveShellInit = optionalString (fishPath != null) ''
-  #    set -g nixos_fish_plugins ${fishPath}
-  #    set -g fisher_path ${fishPath}
-  #    set -p fish_function_path fish_function_path[1] ''$nixos_fish_plugins/functions
-  #    set -p fish_complete_path fish_complete_path[1] ''$nixos_fish_plugins/completions
-
-  #    ${concatMapStringsSep "\n" (drv: ''
-  #      path=${drv.path} package=${drv.name} builtin source ${drv.path}/init.fish 2> /dev/null
-  #    '') pluginDrvs}
-
-  #    for file in ''$nixos_fish_plugins/conf.d/*.fish
-  #        builtin source ''$file 2> /dev/null
-  #    end
-  #  '';
-  #};
   config = mkIf cfg.enable {
     programs.fish.interactiveShellInit = optionalString (fishPath != null) (initScript fishPath);
   };
